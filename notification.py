9d5fef6d884d26d63dbe4940eaea47f796608074
Asirpithanand <99887217+Asirpithanand@users.noreply.github.com>
2022-06-19 20:32:41 +0530

    Update notification.py
    instructions added

--- notification.py
+++ notification.py
@@ -3,9 +3,9 @@ from plyer import battery, notification
 import time
 
 while True:
-    batteryinfo = battery.status   #this will show the current battery percent  and  whether battery is charging or not
+    batteryinfo = battery.status   #this will get the current battery percentage  and  whether battery is charging or not
 
-    # stored the information in separate variable
+    # storing the information in separate variable
     info1 = batteryinfo.get("percentage") 
     info2 = batteryinfo.get("isCharging") 
 
@@ -15,18 +15,18 @@ while True:
                     message= f"{info1}% REMAINING | STATUS UNPLUGGED",
                     app_name='NOTIFY',
                     timeout=15, # the notification will stay on screen for 10 sec
-                    app_icon = "D:\Programing\Language\Python\Projects\issues\Battery Health\Battry_health\Oxygen-Icons.org-Oxygen-Status-battery-charging.ico"  # copy and paste the path of the image attached with this repo
+                    app_icon = " "  # copy and paste the path of the image attached with this repo
                 )
         print('Program Finished')
         break    # breaking the loop once the condition becomes true
 
-    elif info1 >= 90 and info2 == True:
+    elif info1 >= 90 and info2 == True: 
         notification.notify(
                     title='NOTIFY',
                     message= f" BATTERY AT {info1}% | DISCONNECT CHARGER",
                     app_name='NOTIFY',
                     timeout=15, # the notification will stay on screen for 10 sec
-                    app_icon = "D:\Programing\Language\Python\Projects\issues\Battery Health\Battry_health\Oxygen-Icons.org-Oxygen-Status-battery-charging.ico"  # copy and paste the path of the image attached with this repo
+                    app_icon = " "  # copy and paste the path of the image attached with this repo
                 )
         print('Program Finished')
         break    # breaking the loop once the condition becomes true
